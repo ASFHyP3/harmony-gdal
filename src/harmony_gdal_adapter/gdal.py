c6bdad0399ef93d3b146c35e443a19769593547b
Lily Larsen <sglarsen@alaska.edu>
2026-09-15 14:29:09 -0700

    feat: nisar recipe

--- src/harmony_gdal_adapter/gdal.py
+++ src/harmony_gdal_adapter/gdal.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from osgeo.gdal import WarpOptions
-from recipe_types import Recipe
 
 
 def execute_recipe(recipe: Recipe):
@@ -18,7 +17,7 @@ def execute_gdal_warp_recipe(recipe: GdalWarpRecipe):
     raise NotImplementedError()
 
 
-def build_gdal_translate_options(recipe: GdalTransalateRecipe):
+def build_gdal_translate_options(recipe: GdalTranslateRecipe):
     raise NotImplementedError()
 
 
