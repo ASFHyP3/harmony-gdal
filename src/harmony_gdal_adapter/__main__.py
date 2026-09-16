d07b6eb07787391c6370302e06d3f2dc39068a78
ac-johnson <acjohnson149@live.com>
2026-09-15 16:05:12 -0800

    updating cli argparse-dataclass

--- src/harmony_gdal_adapter/__main__.py
+++ src/harmony_gdal_adapter/__main__.py
@@ -1,18 +1,16 @@
-from dataclasses import dataclass, field
-import argparse_dataclass
+from dataclasses import dataclass
 from argparse_dataclass import ArgumentParser
 import argparse
 from recipes import build_recipe,RecipeInputOptions
 from gdal import execute_recipe
-from typing import Literal
 
 def run_cli():     
 
     parser = ArgumentParser(RecipeInputOptions)
-    raise NotImplementedError()
-    
-    # recipe = build_recipe(parser.parse_args())
-    # execute_recipe(recipe)
+    options = parser.parse_args()
+
+    recipe = build_recipe(options)
+    execute_recipe(recipe)
     
 
 if __name__ == '__main__':
