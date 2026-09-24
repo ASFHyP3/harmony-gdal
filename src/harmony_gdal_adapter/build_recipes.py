4d52a5802e6a3a357aa9016e5724ef5fb9172991
Lily Larsen <sglarsen@alaska.edu>
2026-09-24 14:12:31 -0700

    fix: make reprojection and spatial subsetting actually work

--- src/harmony_gdal_adapter/build_recipes.py
+++ src/harmony_gdal_adapter/build_recipes.py
@@ -1,7 +1,7 @@
 """Build a GDAL Recipe and the GDAL Recipe related types."""
 
 import json
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, field
 from importlib.resources import files
 from typing import Literal
 
@@ -18,8 +18,9 @@ class RecipeInputOptions:
     output_type: str
     output_filename: str
     variable_path: str
+    spatial_extents_bounding_box: list[float] | None = field(metadata={'type': float, 'nargs': 4}, default=None)
     target_srs: str | None = None
-    spatial_extents: str | None = None
+    spatial_extents_wkt: str | None = None
 
 
 @dataclass
@@ -93,7 +94,7 @@ class GdalWarpBoundsSpatialSubset:
         Maps directly to GDAL's outputBoundsSRS option in osgeo.gdal.WarpOptions.
     """
 
-    output_bounds: tuple[float, float, float, float]
+    output_bounds: list[float]
     output_bounds_srs: str | None = None
 
 
@@ -219,7 +220,9 @@ def build_recipe(options: RecipeInputOptions) -> Recipe:
     Returns:
         Recipe: The built recipe from the input options
     """
-    recipe_args = [kcl.Argument(name=name, value=value) for name, value in asdict(options).items()]
+    recipe_args = [
+        kcl.Argument(name=name, value=str(value)) for name, value in asdict(options).items() if value is not None
+    ]
     args = kcl.ExecProgramArgs(
         k_filename_list=[str(files(__package__).joinpath('recipes/nisar.k'))],
         args=recipe_args,
--- src/harmony_gdal_adapter/harmony_service.py
+++ src/harmony_gdal_adapter/harmony_service.py
@@ -13,10 +13,6 @@ from harmony_gdal_adapter.build_recipes import RecipeInputOptions, build_recipe
 from harmony_gdal_adapter.gdal import execute_recipe
 
 
-def _bbox_to_wkt(bbox: tuple[float, float, float, float]) -> str:
-    return f'POLYGON(({bbox[0]} {bbox[1]}, {bbox[2]} {bbox[3]}, {bbox[0]}, {bbox[1]}))'
-
-
 class HarmonyAdapter(harmony_service_lib.BaseHarmonyAdapter):
     """Harmony adapter for harmony-gdal-adapter."""
 
@@ -53,12 +49,12 @@ class HarmonyAdapter(harmony_service_lib.BaseHarmonyAdapter):
                 input_filename=granule_filename,
                 output_filename=str(output_path),
                 collection_shortname=str(source.process('shortName')),
-                output_type='GTiff',
+                output_type='COG',
                 variable_path=source.process('variables')[0],
                 target_srs=self.message.format.process('CRS')
                 if self.message.format and self.message.format.crs
                 else None,
-                spatial_extents=_bbox_to_wkt(self.message.subset.process('bbox'))
+                spatial_extents_bounding_box=self.message.subset.process('bbox')
                 if self.message.subset and self.message.subset.bbox
                 else None,
             )
